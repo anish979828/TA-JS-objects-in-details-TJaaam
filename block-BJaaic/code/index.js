7bcf1e21d1bfb7ffb83cb103572b4cc565b59f05
ANISH KUMAR GIRI <girianish2004@gmail.com>
2023-01-30 18:20:40 +0530

    Inheritance

--- block-BJaaic/code/index.js
+++ block-BJaaic/code/index.js
@@ -1,6 +1,9 @@
 // # Inheritance
 
-// Convert the below requirements into inheritance using prototypal patters.
+// Convert the below requirements into inheritance using:
+
+// - Pseudoclassical Pattern
+// - Class Pattern
 
 // #### Animal
 
@@ -11,11 +14,29 @@
 
 // Methods
 
-// - `eat()` - log a message saying `I live in ${location} and I can eat`
-
+// - `eat()- log a message saying `I live in ${location} and I can eat`
 // - `changeLocation(newLocation)` - accepts location and updates the location of the animal
-
 // - `summary()` - returns `I live in ${location} and I have ${numberOfLegs}`
+
+
+function createAnimal(location,numberOfLegs){
+    let obj = Object.create(animalMethods);
+    obj.location = location;
+    obj.numberOfLegs = numberOfLegs;
+    return obj;
+}
+
+let animalMethods = {
+    eat(){
+        console.log(`I live in ${this.location} and I can eat`)
+    },
+    changeLocation(newLocation){
+        this.location = newLocation;
+    },
+    summary(){
+        return `i live in ${this.location} and I have ${this.numberOfLegs} legs`;
+    }
+};
 
 // #### Dog
 
@@ -32,6 +53,31 @@
 // - `changeName(newName)` - accepts the name property and updates the name of the dog
 // - `changeColor(newColor)` - accepts the new color and updates the color of the dog
 // - `summary()` - returns `I am ${name} and I am of ${color} color. I can also bark`
+
+function createDog(location,numberOfLegs,name,color){
+    let obj = createAnimal(location,numberOfLegs);
+    Object.setPrototypeOf(obj,dogMethods);
+    obj.name = name;
+    obj.color = color;
+    return obj;
+};
+
+let dogMethods = {
+    bark: function(){
+        alert(`I am ${this.name} and I can bark🐶`)
+    },
+    changeName: function(newName){
+        this.name = newName;
+    },
+    changeColor: function(newColor){
+        this.color = newColor;
+    },
+    summary: function(){
+        console.log(`I am ${this.name} and I am of ${this.color} color. i can also bark
+        bark`)
+    }
+};
+Object.setPrototypeOf(dogMethods,animalMethods);
 
 // #### Cat
 
@@ -51,3 +97,31 @@
 // - `changeColorOfEyes(newColor)` - accepts the new color and updates the color of the dog
 
 // - `summary()` - returns `I am ${name} and the color of my eyes are ${colorOfEyes}. I can also do meow meow`
+
+function createCat(location,noOfLegs,name,colorOfEyes){
+    let obj = createAnimal(location,noOfLegs);
+    Object.setPrototypeOf(obj,catMethods)
+    obj.name = name;
+    obj.colorOfEyes = colorOfEyes;
+    return obj;
+};
+let catMethods = {
+    meow(){
+        alert(`I am ${this.name} and I can do meow`)
+    },
+    changeName(newName){
+        this.name = newName;
+    },
+    changeColorOfEyes(newColor){
+      this.color = newColor;   
+    },
+    summary(){
+        console.log(`I am ${this.name} and the color of my eyes are ${this.colorOfEyes}. I can do meow meow`)
+
+    }
+};
+Object.setPrototypeOf(catMethods,animalMethods);
+
+
+
+
